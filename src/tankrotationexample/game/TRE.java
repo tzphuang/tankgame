8a4d3cfef9b4d7cb83ebf628f8bc19f736221088
Tony <thuang54@mail.ccsf.edu>
2021-04-21 00:09:46 -0700

    added second tank to TRE

--- src/tankrotationexample/game/TRE.java
+++ src/tankrotationexample/game/TRE.java
@@ -27,6 +27,7 @@ public class TRE extends JPanel implements Runnable {
 
     private BufferedImage world;
     private Tank t1;
+    private Tank t2;
     private Launcher lf;
     private long tick = 0;
 
@@ -41,6 +42,7 @@ public class TRE extends JPanel implements Runnable {
            while (true) {
                 this.tick++;
                 this.t1.update(); // update tank
+                this.t2.update();
                 this.repaint();   // redraw game
                 Thread.sleep(1000 / 144); //sleep for a few milliseconds
                 /*
@@ -62,8 +64,12 @@ public class TRE extends JPanel implements Runnable {
      */
     public void resetGame(){
         this.tick = 0;
-        this.t1.setX(300);
-        this.t1.setY(300);
+
+        this.t1.setX(724);
+        this.t1.setY(468);
+
+        this.t2.setX(300);
+        this.t2.setY(300);
     }
 
 
@@ -77,21 +83,34 @@ public class TRE extends JPanel implements Runnable {
                                        BufferedImage.TYPE_INT_RGB);
 
         BufferedImage t1img = null;
+        BufferedImage t2img = null;
+        BufferedImage unBreakWall = null;
+        BufferedImage breakWall = null;
+
         try {
             /*
              * note class loaders read files from the out folder (build folder in Netbeans) and not the
              * current working directory.
              */
-            t1img = read(Objects.requireNonNull(TRE.class.getClassLoader().getResource("tank1.png")));
+            t1img = read(Objects.requireNonNull(TRE.class.getClassLoader().getResource("tankred.png")));
+            t2img = read(Objects.requireNonNull(TRE.class.getClassLoader().getResource("tankblue.png")));
+            unBreakWall = read(Objects.requireNonNull(TRE.class.getClassLoader().getResource("unbreakable.png")));
+            breakWall = read(Objects.requireNonNull(TRE.class.getClassLoader().getResource("breakable.png")));
+
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
             ex.printStackTrace();
         }
 
-        t1 = new Tank(300, 300, 0, 0, 0, t1img);
+        t1 = new Tank(724, 468, 0, 0, 0, t1img);
         TankControl tc1 = new TankControl(t1, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_ENTER);
         this.setBackground(Color.BLACK);
         this.lf.getJf().addKeyListener(tc1);
+
+        t2 = new Tank (300, 300, 0, 0, 0, t2img);
+        TankControl tc2 = new TankControl(t2, KeyEvent.VK_W, KeyEvent.VK_S, KeyEvent.VK_A, KeyEvent.VK_D, KeyEvent.VK_SPACE);
+        this.setBackground(Color.BLACK);
+        this.lf.getJf().addKeyListener(tc2);
     }
 
 
@@ -102,6 +121,7 @@ public class TRE extends JPanel implements Runnable {
         buffer.setColor(Color.BLACK);
         buffer.fillRect(0,0,GameConstants.GAME_SCREEN_WIDTH,GameConstants.GAME_SCREEN_HEIGHT);
         this.t1.drawImage(buffer);
+        this.t2.drawImage(buffer);
         g2.drawImage(world,0,0,null);
     }
 
